a7a668d4823ab0030eb745a06ac4b0ef37286948
Matheus Maqueda <matheusmaqueda10@gmail.com>
2024-07-05 01:23:28 -0300

    refactor: added the software architecture

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const userRoute = require("./src/routes/user.route");
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+app.use("/soma", userRoute);
 
 app.listen(3000);
+
+// ROTA - Itens Principais:
+    // Method HTTP - CRUD:
+        // GET - Pega um info
+        // POST - Cria um info
+        // PUT - Atualiza TODA a info
+        // PATCH - Altera PARTE da info
+        // DELETE - Deleta um info
+
+    // Name - Identificador da rota "/"
+    // Function (Callback) - Executa algum comando na rota de "name" (req, res) => {}
